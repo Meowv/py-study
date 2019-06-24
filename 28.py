28f9b8345661054a49700f1649a209e1aeb46347
阿星Plus <hackxing@foxmail.com>
2019-06-24 21:04:19 +0800

    send email

--- 28.py
+++ 28.py
@@ -16,7 +16,10 @@ password = input('Password:')
 to_addr = input('To:')
 smtp_server = input('SMTP Server:')
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg = MIMEText('<html><body><h1>Hello</h1>' +
+    '<p>send by <a href="http://www.python.org">Python</a>...</p>' +
+    '</body></html>', 'html', 'utf-8')
 msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
 msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
