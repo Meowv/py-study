202dace6ba9e1a27807e8aa653d5bdb2dad5d804
阿星Plus <hackxing@foxmail.com>
2019-06-24 22:08:21 +0800

    sqlite demo

---
+++ 30.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# 导入SQLite驱动:
+import sqlite3
+# 连接到SQLite数据库
+# 数据库文件是test.db
+# 如果文件不存在，会自动在当前目录创建:
+conn = sqlite3.connect('test.db')
+# 创建一个Cursor:
+cursor = conn.cursor()
+# 执行一条SQL语句，创建user表:
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+# 继续执行一条SQL语句，插入一条记录:
+cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+# 通过rowcount获得插入的行数:
+print(cursor.rowcount)
+
+# 关闭Cursor:
+cursor.close()
+# 提交事务:
+conn.commit()
+# 关闭Connection:
+conn.close()
