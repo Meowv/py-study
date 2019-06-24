71f239f8d240612e4bdd904a84ec4573f4ea88be
阿星Plus <hackxing@foxmail.com>
2019-06-24 21:16:33 +0800

    发送附件

--- 28.py
+++ 28.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from email import encoders
-from email.header import Header
-from email.mime.text import MIMEText
-from email.utils import parseaddr, formataddr
-
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.header import Header
+from email.utils import parseaddr,formataddr
 
 def _format_addr(s):
     name, addr = parseaddr(s)
@@ -16,13 +16,23 @@ password = input('Password:')
 to_addr = input('To:')
 smtp_server = input('SMTP Server:')
 
+# 邮件对象:
+
 # msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
-msg = MIMEText('<html><body><h1>Hello</h1>' +
-    '<p>send by <a href="http://www.python.org">Python</a>...</p>' +
-    '</body></html>', 'html', 'utf-8')
+msg = MIMEMultipart()
 msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
 msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+
+# 邮件正文是MIMEText:
+msg.attach(MIMEText('<html><body><h1>Hello</h1>' +
+    '<p>send by <a href="http://www.python.org">Python</a>...</p>' +
+    '</body></html>', 'html', 'utf-8'))
+
+with open('head.jpg','rb') as f:
+    mime=MIMEImage(f.read())
+    mime.add_header('Content-ID','Imgid')
+    msg.attach(mime)
 
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
