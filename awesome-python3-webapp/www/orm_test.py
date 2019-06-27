d029bb29d878dfb9764352a1cc3abb8a971e2085
阿星Plus <hackxing@foxmail.com>
2019-06-27 21:41:12 +0800

    run app ok

--- awesome-python3-webapp/www/orm_test.py
+++ awesome-python3-webapp/www/orm_test.py
@@ -13,7 +13,7 @@ from models import User, Blog, Comment, next_id
 async def test(loop):
     await orm.create_pool(loop=loop, user='root', password='ABCdef123456', db='awesome')
 
-    u = User(name='123', email='123@meowv.com', passwd='123456', image='about:blank')
+    u = User(name='qix', email='123@meowv.com', passwd='123456', image='about:blank')
     await u.save()
 
 loop = asyncio.get_event_loop()
