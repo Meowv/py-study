7e04595b8633d8537a74be46aa091b9ec8ed6a03
阿星Plus <hackxing@foxmail.com>
2019-06-27 17:57:38 +0800

    config_override.py

---
+++ awesome-python3-webapp/www/config_override.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__author__ = '阿星Plus'
+
+'''
+override configurations
+'''
+
+configs = {
+    'db': {
+        'host': 'rm-uf664xe30hl1i9c09qo.mysql.rds.aliyuncs.com'
+    }
+}
