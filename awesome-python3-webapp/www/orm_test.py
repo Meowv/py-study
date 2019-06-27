60d73e91246b23db838ed82a4d60d82e9d375e79
阿星Plus <hackxing@foxmail.com>
2019-06-27 15:12:18 +0800

    orm_test.py

---
+++ awesome-python3-webapp/www/orm_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__ = '阿星Plus'
+
+'''
+orm test
+'''
+
+import orm
+import asyncio
+from models import User, Blog, Comment, next_id
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user='root', password='ABCdef123456', db='awesome')
+
+    u = User(name='123', email='123@meowv.com', passwd='123456', image='about:blank')
+    await u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.run_forever()
