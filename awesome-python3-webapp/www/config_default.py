f1616b43c7a945de0252c9d554e640423952889c
阿星Plus <hackxing@foxmail.com>
2019-06-27 17:57:27 +0800

    config_default.py

---
+++ awesome-python3-webapp/www/config_default.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__ = '阿星Plus'
+
+'''
+default configurations
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': 'rm-uf664xe30hl1i9c09qo.mysql.rds.aliyuncs.com',
+        'port': 3306,
+        'user': 'root',
+        'password': 'ABCdef123456',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
