6fc93e879ed09572cc3398057ff7aa199e1c723f
阿星Plus <hackxing@foxmail.com>
2019-06-30 20:29:16 +0800

    实现用户的历史记录

---
+++ python-new-journey/py_9.py
@@ -0,0 +1,27 @@
+# 实现用户的历史记录
+
+from random import randint
+from collections import deque
+
+N = randint(0, 100)
+history = deque([],5)
+
+def guess(k):
+    if k == N:
+        print('right')
+        return True
+    if k < N:
+        print('%s is less-than N' % k)
+    else:
+        print('%s is greater-than N' % k)
+    return False
+
+while True:
+    line = input('please input a number:')
+    if line.isdigit():
+        k = int(line)
+        history.append(k)
+        if guess(k):
+            break
+    elif line == 'history' or line == 'h?':
+        print(list(history))
