18eb79f60a78b74afe817bf3a262133c59907bd0
阿星Plus <hackxing@foxmail.com>
2019-07-02 10:25:03 +0800

    读写csv数据

--- python-new-journey/py_26.py
+++ python-new-journey/py_26.py
@@ -1,15 +1,15 @@
 # 读写csv数据
 
-# import csv
+import csv
 
-# with open('python-new-journey/files/demo.csv', 'rb') as rf:
-#     reader = csv.reader(rf)
-#     with open('python-new-journey/files/demo.csv', 'wb') as wf:
-#         writer = csv.writer(wf)
-#         headers = next(reader)
-#         writer.writerow(headers)
-#         for row in reader:
-#             if int(row[2]) < 18:
-#                 break
-#             if row[3] == '男':
-#                 writer.writerow(row)
+with open('python-new-journey/files/demo.csv', 'r', encoding='utf8') as rf:
+    reader = csv.reader(rf)
+    with open('python-new-journey/files/demo_copy.csv', 'w', encoding='utf8') as wf:
+        writer = csv.writer(wf)
+        headers = next(reader)
+        writer.writerow(headers)
+        for row in reader:
+            if int(row[2]) < 20:
+                break
+            if row[3] == '男':
+                writer.writerow(row)
