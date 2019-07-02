8bfcd9e0da43cc74c50a5f93cc5d5b05e8473f3b
阿星Plus <hackxing@foxmail.com>
2019-07-02 15:33:43 +0800

    派生内置不可变类型并修改其实例化行为

--- python-new-journey/py_31.py
+++ python-new-journey/py_31.py
@@ -1,58 +1,14 @@
-# 让对象支持上下文管理
+# 派生内置不可变类型并修改其实例化行为
 
-from telnetlib import telnetlib
-from sys import stdin, stdout
-from collections import deque
+# 实现 __new__ 修改实例化行为
 
-class TelnetClient(object):
-    def __init__(self, addr, port=23):
-        self.addr = addr
-        self.port = port
-        self.tn = None
+class IntTuple(tuple):
+    def __new__(cls, iterable):
+        g = (x for x in iterable if isinstance(x,int) and x > 0)
+        return super(IntTuple, cls).__new__(cls, g)
 
-    def start(self):
-        self.tn = Telnet(self.addr, self.port)
-        self.hidtory = deque()
+    def __init__(self, itetable):
+        super(IntTuple,self).__init__(itetable)
 
-        t = self.tn.read_until('login: ')
-        stdout.write(t)
-        user = stdin.readline()
-        self.tn.write(user)
-
-        t = self.tn.read_until('password: ')
-        if t.startwith(user[:-1]): t = t[len(user) + 1:]
-        stdout.write(t)
-        self.tn.write(stdin.readline())
-
-        t = self.tn.read_until('$ ')
-        stdout.write(t)
-        while True:
-            input = stdin.read()
-            if not input:
-                break
-            self.hidtory.append(input)
-            self.tn.write(input)
-            t = self.tn.read_until('$ ')
-            stdout.write(t[len(input) + 1:])
-
-    def cleanup(self):
-        pass
-
-    def __enter__(self):
-        self.tn = Telnet(self.addr, self.port)
-        self.hidtory = deque()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        print('In __exit__')
-
-        self.tn.close()
-        self.tn = None
-        with open(self.addr + '_history.txt', 'w') as f:
-            f.writelines(self.hidtory)
-        return True
-
-with TelnetClient('127.0.0.1') as client:
-    client.start()
-
-print('end')
+t = IntTuple([1,-1,'abc',6,['x','y'], 3])
+print(t)
