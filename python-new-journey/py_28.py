91a65b58bd054d6e7b3385b073281fe67d7bf75b
阿星Plus <hackxing@foxmail.com>
2019-07-02 14:35:42 +0800

    解析/构建 XML文档

--- python-new-journey/py_28.py
+++ python-new-journey/py_28.py
@@ -1,16 +1,44 @@
-# 解析XML文档
+# 解析/构建 XML文档
 
-from xml.etree.ElementTree import parse
+# from xml.etree.ElementTree import parse
 
-f = open('demo.xml')
+# f = open('demo.xml')
 
-et = parse(f)
+# et = parse(f)
 
-root = et.getroot()
+# root = et.getroot()
 
-for child in root:
-    print(child.get('item'))
+# for child in root:
+#     print(child.get('item'))
+
+# list(root.iter())
 
 # ....
 
-list(root.iter())
+from xml.etree.ElementTree import Element, ElementTree
+from xml.etree.ElementTree import tostring
+
+e = Element('Data')
+print(e.tag)
+
+e.set('name', 'abc')
+print(tostring(e))
+
+e2 = Element('Row')
+
+e3 = Element("Open")
+e3.text = '8.80'
+
+e2.append(e3)
+
+print(tostring(e2))
+
+e.text = None
+
+e.append(e2)
+
+print(tostring(e))
+
+et = ElementTree(e)
+
+et.write('demo.xml')
