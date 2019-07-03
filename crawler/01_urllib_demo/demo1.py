ae00c0805cf8b93304aef4b1ba47beeb83a63235
阿星Plus <hackxing@foxmail.com>
2019-07-03 15:30:01 +0800

    urlencode and parse_qs

--- crawler/01_urllib_demo/demo1.py
+++ crawler/01_urllib_demo/demo1.py
@@ -1,12 +1,36 @@
 from urllib import request
+from urllib import parse
 
+# urlopen
 resp = request.urlopen('http://www.baidu.com')
 
-print(resp.read())
-print(resp.read(100))
-print(resp.readline())
-print(resp.readlines())
-print(resp.getcode())
+# print(resp.read())
+# print(resp.read(100))
+# print(resp.readline())
+# print(resp.readlines())
+# print(resp.getcode())
 
-request.urlretrieve('http://www.baidu.com', 'baidu.html')
-request.urlretrieve('https://s2.ax1x.com/2019/06/23/ZiR4EV.png', 'ms.png')
+# urlretrieve
+# request.urlretrieve('http://www.baidu.com', 'baidu.html')
+# request.urlretrieve('https://s2.ax1x.com/2019/06/23/ZiR4EV.png', 'ms.png')
+
+# urlencode
+params = {'name': '张三', 'age' :18, 'greet': 'hello python'}
+result =  parse.urlencode(params)
+
+print(result)
+
+url = 'http://www.baidu.com/s'
+params = {'wd':'阿星Plus'}
+qs = parse.urlencode(params)
+url = url + '?' + qs
+print(url)
+resp = request.urlopen(url)
+print(resp.read())
+
+# parse_qs
+params = {'name': '张三', 'age' :18, 'greet': 'hello python'}
+qs = parse.urlencode(params)
+print(qs)
+result = parse.parse_qs(qs)
+print(result)
