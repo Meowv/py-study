d37fcb56fcddf3db263e6bcf5af8c9c69edc214f
阿星Plus <hackxing@foxmail.com>
2019-07-04 16:04:27 +0800

    优化代码

--- crawler/dytt_spider/main.py
+++ crawler/dytt_spider/main.py
@@ -3,15 +3,29 @@ import requests
 
 BASE_DOMAIN = 'https://www.dytt8.net'
 
-url = 'https://www.dytt8.net/html/gndy/dyzz/list_23_1.html'
-headers = {
+HEADERES = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 }
-response = requests.get(url, headers=headers)
 
-text = response.content.decode('gbk')
-html = etree.HTML(text)
+def get_detail_urls(url):
+    response = requests.get(url, headers=HEADERES)
 
-detail_urls = html.xpath("//table[@class='tbspan']//a/@href")
-for detail_url in detail_urls:
-    print(BASE_DOMAIN+detail_url)
+    text = response.content.decode('gbk')
+    html = etree.HTML(text)
+
+    detail_urls = html.xpath("//table[@class='tbspan']//a/@href")
+    detail_urls = map(lambda url: BASE_DOMAIN + url, detail_urls)
+
+    return detail_urls
+
+def parse_detail_page(url):
+    pass
+
+def spider():
+    base_url = 'https://www.dytt8.net/html/gndy/dyzz/list_23_{}.html'
+    for x in range(1, 198):
+        url = base_url.format(x)
+        movie = parse_detail_page(url)
+
+if __name__ == "__main__":
+    spider()
