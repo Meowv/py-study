2e80726b5b05df69a79f4f4ca95479872a9a9da2
阿星Plus <hackxing@foxmail.com>
2019-07-05 13:08:18 +0800

    中国天气网数据抓取

---
+++ crawler/weather_spider/main.py
@@ -0,0 +1,10 @@
+# http://www.weather.com.cn/textFC/hb.shtml 华北
+# http://www.weather.com.cn/textFC/db.shtml 东北
+# http://www.weather.com.cn/textFC/hd.shtml 华东
+# http://www.weather.com.cn/textFC/hz.shtml 华中
+# http://www.weather.com.cn/textFC/hn.shtml 华南
+# http://www.weather.com.cn/textFC/xb.shtml 西北
+# http://www.weather.com.cn/textFC/xn.shtml 西南
+# http://www.weather.com.cn/textFC/gat.shtml 港澳台
+
+from bs4 import BeautifulSoup
