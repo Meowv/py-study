568c70c46d9378d8e3e6ae5cda6e5eb499d7f231
阿星Plus <hackxing@foxmail.com>
2019-07-06 11:20:22 +0800

    中国天气网所有城市最低气温数据

--- crawler/weather_spider/main.py
+++ crawler/weather_spider/main.py
@@ -1,10 +1,44 @@
-# http://www.weather.com.cn/textFC/hb.shtml 华北
-# http://www.weather.com.cn/textFC/db.shtml 东北
-# http://www.weather.com.cn/textFC/hd.shtml 华东
-# http://www.weather.com.cn/textFC/hz.shtml 华中
-# http://www.weather.com.cn/textFC/hn.shtml 华南
-# http://www.weather.com.cn/textFC/xb.shtml 西北
-# http://www.weather.com.cn/textFC/xn.shtml 西南
-# http://www.weather.com.cn/textFC/gat.shtml 港澳台
-
 from bs4 import BeautifulSoup
+import requests
+
+def parse_page(url):
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    }
+    response = requests.get(url, headers=HEADERS)
+    text = response.content.decode('utf-8')
+
+    # soup = BeautifulSoup(text, 'lxml')
+    # pip install html5lib
+    soup = BeautifulSoup(text, 'html5lib')
+
+    conMidtab = soup.find('div', class_='conMidtab')
+    tables = conMidtab.find_all('table')
+    for table in tables:
+        trs = table.find_all('tr')[2:]
+        for index, tr in enumerate(trs):
+            tds = tr.find_all('td')
+            city_td = tds[0]
+            if index == 0:
+                city_td = tds[1]
+            city = list(city_td.stripped_strings)[0]
+            temp_td = tds[-2]
+            min_temp = list(temp_td.stripped_strings)[0]
+            print({"city":city,"min_temp":min_temp})
+
+def main():
+    urls = [
+        'http://www.weather.com.cn/textFC/hb.shtml',
+        'http://www.weather.com.cn/textFC/db.shtml',
+        'http://www.weather.com.cn/textFC/hd.shtml',
+        'http://www.weather.com.cn/textFC/hz.shtml',
+        'http://www.weather.com.cn/textFC/hn.shtml',
+        'http://www.weather.com.cn/textFC/xb.shtml',
+        'http://www.weather.com.cn/textFC/xn.shtml',
+        'http://www.weather.com.cn/textFC/gat.shtml'
+    ]
+    for url in urls:
+        parse_page(url)
+
+if __name__ == "__main__":
+    main()
