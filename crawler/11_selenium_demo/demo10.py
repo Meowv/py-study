f97504696deab862804263756824d273f0881472
阿星Plus <hackxing@foxmail.com>
2019-07-09 17:24:05 +0800

    selenium中的WebElement类

---
+++ crawler/11_selenium_demo/demo10.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+
+driver_path = r'D:\Program Files\chromedriver\chromedriver.exe'
+driver = webdriver.Chrome(executable_path=driver_path)
+driver.get('https://www.baidu.com/')
+
+submitBtn = driver.find_element_by_id('su')
+print(type(submitBtn))
+
+print(submitBtn.get_attribute('value'))
+
+driver.save_screenshot('baidu.png')
