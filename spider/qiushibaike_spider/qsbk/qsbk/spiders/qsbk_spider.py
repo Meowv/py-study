86af49d1e70dea382846c4de3b03f12ddd1b97d6
阿星Plus <hackxing@foxmail.com>
2019-07-11 23:06:36 +0800

    糗事百科之抓取多个页面完成

--- spider/qiushibaike_spider/qsbk/qsbk/spiders/qsbk_spider.py
+++ spider/qiushibaike_spider/qsbk/qsbk/spiders/qsbk_spider.py
@@ -2,6 +2,7 @@
 import scrapy
 from scrapy.http.response.html import HtmlResponse
 from scrapy.selector.unified import SelectorList
+
 from qsbk.items import QsbkItem
 
 
@@ -9,6 +10,7 @@ class QsbkSpiderSpider(scrapy.Spider):
     name = 'qsbk_spider'
     allowed_domains = ['qiushibaike.com']
     start_urls = ['https://www.qiushibaike.com/text/page/1/']
+    base_domain = 'https://www.qiushibaike.com'
 
     def parse(self, response):
         duanziDivs = contentLeft = response.xpath("//div[@id='content-left']/div")
@@ -22,3 +24,8 @@ class QsbkSpiderSpider(scrapy.Spider):
 
             item = QsbkItem(author=author,content=content)
             yield item
+        next_url = response.xpath("//ul[@class='pagination']/li[last()]/a/@href").get()
+        if not next_url:
+            return
+        else:
+            yield scrapy.Request(self.base_domain + next_url, self.parse)
