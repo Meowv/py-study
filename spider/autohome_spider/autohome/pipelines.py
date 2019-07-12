371ec9cf5b3b5d77668b1c46f6103d2c6ecf97dd
阿星Plus <hackxing@foxmail.com>
2019-07-12 20:12:02 +0800

    urlretrieve下载图片

--- spider/autohome_spider/autohome/pipelines.py
+++ spider/autohome_spider/autohome/pipelines.py
@@ -5,7 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
+from urllib import request
 
 class AutohomePipeline(object):
+    def __init__(self):
+        self.path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images')
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+        
     def process_item(self, item, spider):
+        category = item["category"]
+        urls = item["urls"]
+
+        category_path = os.path.join(self.path, category)
+        if not os.path.exists(category_path):
+            os.mkdir(category_path)
+        for url in urls:
+            img_name = url.split('_')[-1]
+            request.urlretrieve(url, os.path.join(category_path, img_name))
         return item
