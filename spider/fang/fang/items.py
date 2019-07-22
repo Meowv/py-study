ab906a928d5978ee8fcf85d3d3ddee79b1a43e09
阿星Plus <hackxing@foxmail.com>
2019-07-22 23:23:05 +0800

    房天下全国658城市新房和二手房房源信息抓取

---
+++ spider/fang/fang/items.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+
+class NewHouseItem(scrapy.Item):
+    # 省份
+    province = scrapy.Field()
+    # 城市
+    city = scrapy.Field()
+    # 小区名字
+    name = scrapy.Field()
+    # 价格
+    price = scrapy.Field()
+    # 几居
+    rooms = scrapy.Field()
+    # 面积
+    area = scrapy.Field()
+    # 地址
+    address = scrapy.Field()
+    # 行政区
+    district = scrapy.Field()
+    # 是否在售
+    sale = scrapy.Field()
+    # 详情页面
+    origin_url = scrapy.Field()
